585bf70072fe03ec72daca2227a6de157e151669
itsmejustino <justindortiz@me.com>
2023-01-09 15:45:44 -0700

    image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -139,12 +139,13 @@ export default Home;
 
 const AuthShowcase: React.FC = () => {
   const { data: sessionData } = useSession();
+  const myImage = sessionData && sessionData.user?.image
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl text-white">
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
       </p>
-      {sessionData && <img src="{{sessionData.user?.image}}"/>}
+      {sessionData && <img src="{{myImage}}"/>}
       <button
         className="rounded-full bg-grey/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
         onClick={sessionData ? () => signOut() : () => signIn()}

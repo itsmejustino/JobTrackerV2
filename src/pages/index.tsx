1920e0ee4bf13941743af5cc9de94b841e078464
itsmejustino <justindortiz@me.com>
2023-01-09 15:38:28 -0700

    image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router';
 import { api } from "../utils/api";
 import JobList from "./components/Joblist";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Image from 'next/image'
 
 //1. Capture input in State object. [DONE]
 //2. Use object in State to Mutate to database. [DONE]
@@ -138,11 +139,10 @@ export default Home;
 
 const AuthShowcase: React.FC = () => {
   const { data: sessionData } = useSession();
-
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}{sessionData.user?.image }</span>}
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span> && <img src="{{sessionData.user?.image}}"/>}
       </p>
       <button
         className="rounded-full bg-grey/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"

6318a8a43294ca0d429df6423c565dd6f2774344
itsmejustino <justindortiz@me.com>
2023-01-13 13:01:09 -0700

    new loader

--- src/pages/components/Joblist.tsx
+++ src/pages/components/Joblist.tsx
@@ -22,34 +22,64 @@ const JobList: FC = () => {
   };
 
   const userId = sessionData?.user?.id;
-  if (!userId || !queryUserJobList.data || queryUserJobList.error || queryUserJobList.isLoading) return <div className="flex justify-center"> <p className="flex justify-center">You need to be signed in to use the Job Tracker...</p> </div>;
-  if (userId && queryUserJobList.isLoading) return <div className="flex justify-center bg-slate-700"> <p className="flex justify-center">
-    <svg version="1.1" id="L6" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-  viewBox="0 0 100 100" enable-background="new 0 0 100 100" xmlSpace="preserve">
-   <rect fill="none" stroke="#fff" stroke-width="4" x="25" y="25" width="50" height="50">
-  <animateTransform
-     attributeName="transform"
-     dur="0.5s"
-     from="0 50 50"
-     to="180 50 50"
-     type="rotate"
-     id="strokeBox"
-     attributeType="XML"
-     begin="rectBox.end"/>
-  </rect>
-   <rect x="27" y="27" fill="#fff" width="46" height="50">
-  <animate
-     attributeName="height"
-     dur="1.3s"
-     attributeType="XML"
-     from="50" 
-     to="0"
-     id="rectBox" 
-     fill="freeze"
-     begin="0s;strokeBox.end"/>
-  </rect>
-</svg>
-Loading job list...</p> </div>;
+  if (
+    !userId ||
+    !queryUserJobList.data ||
+    queryUserJobList.error ||
+    queryUserJobList.isLoading
+  )
+    return (
+      <div className="flex justify-center bg-slate-700">
+      {" "}
+      <p className="flex justify-center">
+        <svg
+          version="1.1"
+          id="L6"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          viewBox="0 0 100 100"
+          enable-background="new 0 0 100 100"
+          xmlSpace="preserve"
+        >
+          <rect
+            fill="none"
+            stroke="#fff"
+            stroke-width="4"
+            x="25"
+            y="25"
+            width="50"
+            height="50"
+          >
+            <animateTransform
+              attributeName="transform"
+              dur="0.5s"
+              from="0 50 50"
+              to="180 50 50"
+              type="rotate"
+              id="strokeBox"
+              attributeType="XML"
+              begin="rectBox.end"
+            />
+          </rect>
+          <rect x="27" y="27" fill="#fff" width="46" height="50">
+            <animate
+              attributeName="height"
+              dur="1.3s"
+              attributeType="XML"
+              from="50"
+              to="0"
+              id="rectBox"
+              fill="freeze"
+              begin="0s;strokeBox.end"
+            />
+          </rect>
+        </svg>
+        Loading job list...
+      </p>{" "}
+    </div>
+    );
 
   const displayJobs = queryUserJobList.data
     ?.filter((job) => job.userId === userId)

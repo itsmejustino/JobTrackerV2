e870d978430b4a9e15459b134ebf7382026c7fb0
itsmejustino <justindortiz@me.com>
2023-01-12 17:13:36 -0700

    remove second job name input field

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -109,19 +109,6 @@ const Home: NextPage = () => {
           className="mt-20 flex flex-row flex-wrap items-end justify-center gap-4"
           onSubmit={getInput}
         >
-
-          <div className="flex flex-col gap-0">
-            <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
-              Job Name
-            </label>
-            <input
-              name="userId"
-              type="text"
-              id="small-input"
-              className="w-100 block rounded-lg border border-gray-300 bg-gray-50 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-xs"
-            />
-          </div>
-
           <div className="flex flex-col gap-0">
             <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
               Job Name

74a43c976105e485333c1f509b67aea9382950b0
itsmejustino <justindortiz@me.com>
2023-01-13 14:28:29 -0700

    edit click and save click

--- src/pages/components/Joblist.tsx
+++ src/pages/components/Joblist.tsx
@@ -17,6 +17,12 @@ const JobList: FC = () => {
   const queryUserJobList = api.jobs.getAllUserJobs.useQuery();
   const editJobMutation = api.jobs.editJob.useMutation().mutateAsync;
   const userId = sessionData?.user?.id;
+
+  const [showEditInput, setShowEditInput] = useState(false);
+
+  const handleEditClick = () => {
+    setShowEditInput(!showEditInput);
+  };
   const [formData, setFormData] = useState<FormData>({
     jobName: "",
     company: "",
@@ -55,11 +61,18 @@ const JobList: FC = () => {
     platform: string,
     appliedon: string,
     interview: string,
-    followup: string,
+    followup: string
   ): Promise<void> => {
-    editJobMutation(
-      { id, userId, jobName, company, platform, appliedon, interview, followup }
-    ).then(() => {
+    editJobMutation({
+      id,
+      userId,
+      jobName,
+      company,
+      platform,
+      appliedon,
+      interview,
+      followup,
+    }).then(() => {
       queryUserJobList.refetch();
     });
   };
@@ -184,64 +197,100 @@ const JobList: FC = () => {
                 />
               </svg>
             </button>
-            <input
-              type="text"
-              name="jobName"
-              value={formData.jobName}
-              onChange={(event) =>
-                handleChange(event as React.ChangeEvent<HTMLInputElement>)
-              }
-            />
-            <input
-              type="text"
-              name="company"
-              value={formData.company}
-              onChange={(event) =>
-                handleChange(event as React.ChangeEvent<HTMLInputElement>)
-              }
-            />
-            <input
-              type="text"
-              name="platform"
-              value={formData.platform}
-              onChange={(event) =>
-                handleChange(event as React.ChangeEvent<HTMLInputElement>)
-              }
-            />
-            <input
-              type="text"
-              name="appliedon"
-              value={formData.appliedon}
-              onChange={(event) =>
-                handleChange(event as React.ChangeEvent<HTMLInputElement>)
-              }
-            />
-                <input
-              type="text"
-              name="interview"
-              value={formData.interview}
-              onChange={(event) =>
-                handleChange(event as React.ChangeEvent<HTMLInputElement>)
-              }
-            />
-                   <input
-              type="text"
-              name="followup"
-              value={formData.followup}
-              onChange={(event) =>
-                handleChange(event as React.ChangeEvent<HTMLInputElement>)
-              }
-            />
+            <div onChange={handleEditClick}>
+              <input
+                type="text"
+                name="jobName"
+                value={formData.jobName}
+                onChange={(event) =>
+                  handleChange(event as React.ChangeEvent<HTMLInputElement>)
+                }
+              />
+              <input
+                type="text"
+                name="company"
+                value={formData.company}
+                onChange={(event) =>
+                  handleChange(event as React.ChangeEvent<HTMLInputElement>)
+                }
+              />
+              <input
+                type="text"
+                name="platform"
+                value={formData.platform}
+                onChange={(event) =>
+                  handleChange(event as React.ChangeEvent<HTMLInputElement>)
+                }
+              />
+              <input
+                type="date"
+                name="appliedon"
+                value={formData.appliedon}
+                onChange={(event) =>
+                  handleChange(event as React.ChangeEvent<HTMLInputElement>)
+                }
+              />
+              <input
+                type="date"
+                name="interview"
+                value={formData.interview}
+                onChange={(event) =>
+                  handleChange(event as React.ChangeEvent<HTMLInputElement>)
+                }
+              />
+              <input
+                type="date"
+                name="followup"
+                value={formData.followup}
+                onChange={(event) =>
+                  handleChange(event as React.ChangeEvent<HTMLInputElement>)
+                }
+              />
+
+              <button
+                onClick={() => {
+                  editJob(
+                    x.id,
+                    userId,
+                    formData.jobName,
+                    formData.company,
+                    formData.platform,
+                    formData.appliedon,
+                    formData.interview,
+                    formData.followup
+                  );
+                }}
+                type="button"
+                className="m-4 flex flex-row justify-center gap-2 rounded-md bg-blue-400 p-2 text-sm shadow-md transition hover:bg-blue-500"
+                key={x.id}
+              >
+                Save
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+              </button>
+            </div>
 
             <button
               onClick={() => {
-                editJob(x.id, userId, formData.jobName, formData.company, formData.platform, formData.appliedon, formData.interview, formData.followup);
+                handleEditClick
               }}
               type="button"
               className="m-4 flex flex-row justify-center gap-2 rounded-md bg-blue-400 p-2 text-sm shadow-md transition hover:bg-blue-500"
               key={x.id}
             >
-              Delete
+              Edit
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"

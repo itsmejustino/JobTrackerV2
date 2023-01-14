ab6f2f9f57d641ca845c982722e8236b50612bc7
itsmejustino <justindortiz@me.com>
2023-01-13 17:56:03 -0700

    space around

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -210,7 +210,7 @@ const Home: NextPage = () => {
           </button>
         </form>
       </main>
-      <section className="flex justify-center gap-2">{<JobList />}</section>
+      <section className="flex justify-around gap-2">{<JobList />}</section>
     </>
   );
 };
